80f28549a79bb73469435e90db98e23e3fcdb208
mrred <zarubinr85@gmail.com>
2026-02-21 20:54:00 +0700

    test

--- CompGraphLab2/Source.cpp
+++ CompGraphLab2/Source.cpp
@@ -17,7 +17,7 @@ void display_2_0()
 
 	glBegin(GL_POINTS);
 
-	glVertex2f(-3, 4);
+	glVertex2i(-3, 4);
 
 	glEnd();
 }
@@ -31,29 +31,66 @@ void display_2_1()
 
 	glBegin(GL_POINTS);
 
-	glVertex2f(3, 3);
+	glVertex2i(3, 3);
 
 	glEnd();
 }
 
 void display_2_2()
 {
+	glViewport(400, 200, 200, 200);
+
+	glLineWidth(1); 
+
+
+	glBegin(GL_LINE_LOOP);
+
+	glColor3ub(255, 255, 255);
+	
+	glVertex2f(-5, 4);
+	glVertex2f(-3, 5);
+	glVertex2f(-3, 3);
+	glVertex2f(-5, 2);
+	glVertex2f(-2, 5);
+	glVertex2f(-2, 2);
+
+
+	glEnd();
 
 }
 
 void display_2_3()
 {
-
+	
 }
 
 void display_3_0()
 {
+	glViewport(0, 0, 200, 200);
 
+	glColor3ub(0, 0, 255);
+	glPointSize(18.0);
+	glEnable(GL_POINT_SMOOTH);
+
+	glBegin(GL_POINTS);
+
+	glVertex2i(-5, -5);
+
+	glEnd();
 }
 
 void display_3_1()
 {
+	glViewport(200, 0, 200, 200);
 
+	glColor3ub(0, 255, 0);
+	glPointSize(8.0);
+
+	glBegin(GL_POINTS);
+
+	glVertex2i(3, -4);
+
+	glEnd();
 }
 
 void display_3_2_3_3()
@@ -65,6 +102,12 @@ void  display(void)
 {
 	display_2_0();
 	display_2_1();
+	display_2_2();
+	display_2_3();
+	display_3_0();
+	display_3_1();
+	display_3_2_3_3();
+
 
 	glFlush();
 
@@ -79,7 +122,7 @@ void init(void)
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-24.0, 24.0, -30.0, 30.0, 2.0, 12.0);
+	glOrtho(-6.0, 6.0, -6.0, 6.0, 2.0, 12.0);
 	glMatrixMode(GL_MODELVIEW);
 	gluLookAt(
 		0, 0, 5, 
